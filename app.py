d9bf8ddaf96b573970f7deec8c74ece0c2f12374
dagmuzik <dagmuzik@gmail.com>
2025-05-11 17:46:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,17 @@
-import os
 from flask import Flask, render_template, request
-from product_scraper import scrape_kicks_product
+from sneakerhunt import buscar_todos
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    resultado = None
-    mensaje = ""
-    if request.method == "POST":
-        url = request.form.get("url")
-        talla = request.form.get("talla", "9.5")
-        if url:
-            producto = scrape_kicks_product(url, talla)
-            if producto:
-                resultado = [producto]  # lo mandamos como lista para renderizar igual
-            else:
-                mensaje = f"No se encontró la talla {talla} en el producto proporcionado."
+    talla = request.values.get('talla', '9.5')
+    
+    resultados = buscar_todos(talla)
 
-    return render_template("index.html",
-                           productos=resultado,
-                           mensaje=mensaje)
+    productos = resultados.to_dict(orient='records')
 
-if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    return render_template('index.html', productos=productos, talla=talla)
 
+if __name__ == '__main__':
+    app.run(debug=True)
