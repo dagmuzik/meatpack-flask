53f4ca5ad2e05c7aa60e91ee76e4a68a1eb466b6
dagmuzik <dagmuzik@gmail.com>
2025-05-18 14:25:14 -0600

    Update sneakerhunt.py

--- sneakerhunt.py
+++ sneakerhunt.py
@@ -672,7 +672,8 @@ def ejecutar_todo():
 
     # Shopify stores
     scrap_raw_shopify()
-    obtener_shopify(url, tienda, talla)
+    generar_cache_estandar_desde_raw()
+
     
     # Otras tiendas (VTEX, Woo, etc.)
     tiendas = {
