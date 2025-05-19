816117c5c05250f205fe97b7bf0b51c46b04538c
dagmuzik <dagmuzik@gmail.com>
2025-05-19 10:12:12 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ import requests
 from datetime import datetime
 
 # Encabezados HTTP comunes
-headers = {
+HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
     "Accept": "application/json",
     "Referer": "https://deporteselcentro.com/",
